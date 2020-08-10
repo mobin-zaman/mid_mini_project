fd8048d8467989767f59b7e6dfdd948dbe388571
mobin_zaman <mobinzamandev@gmail.com>
2020-08-10 13:38:11 +0600

    registration handler completed

--- registration_handler.php
+++ registration_handler.php
@@ -1,5 +1,7 @@
 <?php
 
+include ('db_connection.php');
+
     if(isset($_POST['name']) &&
     isset($_POST['email']) &&
     isset($_POST['username']) &&
@@ -8,7 +10,20 @@
     isset($_POST['gender']) &&
         isset($_POST['dd']) && isset($_POST['mm']) && isset($_POST['yy'])
     ) {
-        
+
+        signup($_POST['name'], $_POST['email'], $_POST['username'], $_POST['password'], $_POST['confirmpassword'], $_POST['gender'], $_POST['dd'], $_POST['yy'], $_POST['mm']);
+
     } else {
         die("you need to provide all the information");
+    }
+
+
+    function signup($name, $email, $username, $password,$confirmpassword, $gender, $dd, $mm, $yy) {
+        $connection = new createCon();
+        $connection->connect();
+
+        $query = "insert into user values (?,?,?,?,?,?,?)";
+        $stmt = $connection->prepare($query);
+        $stmt->bind_param("sssssss", $name, $email, $username, $password, $confirmpassword, $gender, $dd.$mm.$yy);
+        $stmt->execute();
     }
